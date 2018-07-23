8588d08e5e8e598d2d2da3be2a825b73986c9412
MegatronusPrime <nailahharvey2003@gmail.com>
2018-07-23 23:10:04 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -1,33 +1,16 @@
-puts "How To Do The Macarena:"
-puts ""
+#Syntax:
 
-# Fix The Dance Instructions Below
-# Every line should have one instruction using puts.
-# The first instruction is correct. 
-
+def macarena
 # Step 1: GOOD
-puts "Step 1: Right arm out in front of you, palm facing down."
+  puts "Step 1: Right arm out in front of you, palm facing down."
+  puts "Step 2: Left arm out in front of you, palm facing down."
+  puts "Step 3: Place your right hand on your left shoulder."
+  puts "Step 4: Place your left arm on your right shoulder."
+  puts "Step 5: Bring your right hand up and around to the back of your head."
+  puts "Step 6: Bring your left hand up and around to the back of your head."
+  puts "Step 7: Place right hand on hip."
+  puts "Step 8: Place left hand on hip."
+  puts "Say: 'AY! Macarena!'"
+end
 
-# Step 2: FIXME
-puts "Step 2: Left arm out in front of you, palm facing down."
-
-# Step 3: FIXME
-puts "Step 3: Place your right hand on your left shoulder."
-
-# Step 4: FIXME
-puts "Step 4: Place your left arm on your right shoulder."
-
-# Step 5: FIXME
-puts "Step 5: Bring your right hand up and around to the back of your head."
-
-# Step 6: FIXME
-puts "Step 6: Bring your left hand up and around to the back of your head."
-
-# Step 7: FIXME
-puts "Step 7: Place right hand on hip."
-
-# Step 8: FIXME
-puts "Step 8: Place left hand on hip."
-
-# Step 9: FIXME
-puts "Say: 'AY! Macarena!'"
+macarena
