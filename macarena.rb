7d44a7724890878e8f7b8843344c0d9507e6fd75
MegatronusPrime <nailahharvey2003@gmail.com>
2018-07-23 23:01:52 +0000

    automatically backed up by learn

--- macarena.rb
+++ macarena.rb
@@ -9,22 +9,24 @@ puts ""
 puts "Step 1: Right arm out in front of you, palm facing down."
 
 # Step 2: FIXME
-puts "Step 2: Turn your right palm up."
+puts "Step 2: Left arm out in front of you, palm facing down."
 
 # Step 3: FIXME
 puts "Step 3: Place your right hand on your left shoulder."
 
 # Step 4: FIXME
-puts "Step 4: Turn your left palm up."
+puts "Step 4: Place your left arm on your right shoulder."
 
 # Step 5: FIXME
 puts "Step 5: Bring your right hand up and around to the back of your head."
 
 # Step 6: FIXME
-puts "Step 6: Left arm out in front of you, palm facing down."
+puts "Step 6: Bring your left hand up and around to the back of your head."
 
 # Step 7: FIXME
-puts "Step 7: Place your left hand on your left shoulder."
+puts "Step 7: Place right hand on hip."
 
 # Step 8: FIXME
-puts "Step 8: The Last Step"
+puts "Step 8: Place left hand on hip."
+
+puts "Say: 'AY! Macarena!'"
